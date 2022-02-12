dd0424ddbcc283db2465ed47c4decab426d40549
T33jaay <ericsson5050@hotmail.com>
2022-02-12 12:44:12 +0530

    added tailwind, poke api and search

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>About Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p>A demo application built using Svelte Kit and the Pokemin API</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { each } from 'svelte/internal';
+	import PokeCard from '../components/pokeCard.svelte';
+	import { pokemon } from '../stores/pokestore';
+
+	let searchTerm = '';
+	let filteredPokemon = [];
+
+	$: {
+		if (searchTerm) {
+			filteredPokemon = $pokemon.filter((pokeman) =>
+				pokeman.name.toLowerCase().includes(searchTerm.toLowerCase())
+			);
+		} else {
+			filteredPokemon = [...$pokemon];
+		}
+	}
+</script>
+
+<svelte:head>
+	<title>Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">Welcome to Svelte Kit Pokedex!</h1>
+<input
+	class="w-full rounded-md text-lg p-4 border-2 border-gray-200"
+	type="text"
+	placeholder="Search Pokemon"
+	bind:value={searchTerm}
+/>
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+	{#each filteredPokemon as pokeman}
+		<PokeCard {pokeman} />
+	{/each}
+</div>
